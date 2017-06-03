7151e3b17ac82e3fe0a1265539b5777a751bb3b2
Nathan Mayer <nm.develops@gmail.com>
2017-06-02 23:11:00 -0700

    Add radio buttons for function selection, not yet pointed to functions

--- js/scripts.js
+++ js/scripts.js
@@ -22,10 +22,19 @@ var remainder = function(number1, number2) {
 
 //this is our user interface
 
-$(function) {
-  $("#blanks form").submit(function(event) {
-    add (input#number1, input#number2);
+$(document).ready(function() {
+  $("#variables form").submit(function(event) {
+    var number1 = parseFloat($("input#number1").val());
+    var number2 = parseFloat($("input#number2").val());
+    var operator = $("input:radio[name=operator]:checked").val();
 
+    console.log("1st number: " + number1);  // for debugging
+    console.log("2nd number: " + number2);  // for debugging
+    console.log("operator: " + operator);  // for debugging
+
+    var output =  add(number1, number2);
+
+    $(".result").text(output);
     event.preventDefault();
   });
 });
